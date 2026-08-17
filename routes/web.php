7321a58a263a6495f3000ffb29b812f76dd3bc0e
Joel <joel@example.com>
2026-08-17 16:19:55 +0800

    Fix web.php syntax and define bookings.store route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route for form submissions
+Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');
