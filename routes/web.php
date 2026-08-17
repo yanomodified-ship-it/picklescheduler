496471696563fe98d10ccab71dc29fdeeb8c37a4
yanomodified-ship-it <yanomodified@gmail.com>
2026-08-17 20:54:09 +0800

    Fix route name mismatch for booking store action

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
 Route::get('/', [BookingController::class, 'index'])->name('home');
-Route::post('/bookings', [BookingController::class, 'store'])->name('bookings.store');
+Route::post('/bookings', [BookingController::class, 'store'])->name('booking.store');
 Route::get('/confirmation/{booking_reference}', [BookingController::class, 'confirmation'])->name('bookings.confirmation');
