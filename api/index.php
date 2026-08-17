3cdd7add99998451c048067b38bce397db92f085
yanomodified-ship-it <yanomodified@gmail.com>
2026-08-17 16:44:04 +0800

    Update git author configuration and deployment settings

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Copy SQLite database to writable /tmp directory in serverless environment
+// Copy SQLite file to writable runtime temp directory
 if (!file_exists('/tmp/database.sqlite') && file_exists(__DIR__ . '/../database/database.sqlite')) {
     copy(__DIR__ . '/../database/database.sqlite', '/tmp/database.sqlite');
 }
